98ad8c1ee8e30ac6a57a9578c1334a7c1229e738
TheBenitoo <benitograuel@googlemail.com>
2021-01-10 15:59:18 +0100

    Added PressButtonTrigger for MissionStart

--- DriverGame/Assets/Scripts/Missions/StartMissionTrigger.cs
+++ DriverGame/Assets/Scripts/Missions/StartMissionTrigger.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class StartMissionTrigger : MonoBehaviour
 {
     public bool hasMissionStarted = false;
+    [SerializeField] public GameObject uiElement;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player_Car")
         {
-            hasMissionStarted = true;
+            uiElement.SetActive(true);
 
-            Time.timeScale = 0.5f;
-            InvokeRepeating("returnToNormalSpeed", 1f, 0.5f);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                hasMissionStarted = true;
+                uiElement.SetActive(false);
+
+                Time.timeScale = 0.5f;
+                InvokeRepeating("returnToNormalSpeed", 1f, 0.5f);
+            }
         }
     }
     void returnToNormalSpeed()
     {
         Time.timeScale = 1f;
         CancelInvoke();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player_Car")
+        {
+            uiElement.SetActive(false);
+        }
     }
 }
