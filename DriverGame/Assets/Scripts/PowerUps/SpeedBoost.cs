7ceffaeb6caafa34b1fb3a9df27afb91e9a7db13
Alejandro Restrepo Klinge <56825548+mrrestre@users.noreply.github.com>
2021-02-19 14:01:13 +0100

    Last commit adds

---
+++ DriverGame/Assets/Scripts/PowerUps/SpeedBoost.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour
+{
+    public float accelerationRate = 16f;
+    public float finalVelocity = 70f;
+    public float howLong = 4f;
+}
